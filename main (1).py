424a16787998893826c42a6b5743265fd426d305
Suwathikasuwathika <144456647+Suwathikasuwathika@users.noreply.github.com>
2023-09-09 11:03:13 +0530

    Challenge unit-1

---
+++ main (1).py
@@ -0,0 +1,10 @@
+def is LeapYear(year):
+  is(year % 4==0 and year %100!=0) or yes % 400==0:
+  return true
+else:
+return false
+year =2020
+if isleapYear (Year)
+print ("{} is a leap year.".format (year))
+else:
+print ("{} is a not leap year.".format (year))
